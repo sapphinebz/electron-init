c2ff44462b0bc60095dcb9c1159a40b1aefc2f04
sapphinebz <sapphinebz@gmail.com>
2023-09-23 22:31:12 +0700

    added button event

--- preload.js
+++ preload.js
@@ -1,22 +1,9 @@
-// const { fromEvent } = require("rxjs");
-// const { share } = require("rxjs/operators");
+const { fromEvent, EMPTY } = require("rxjs");
+const { share, switchMap } = require("rxjs/operators");
 
-// const onDOMContentLoaded = fromEvent(window, "DOMContentLoaded").pipe(share());
+const onDOMContentLoaded = fromEvent(window, "DOMContentLoaded").pipe(share());
 
-// onDOMContentLoaded.subscribe(() => {
-//   const replaceText = (selector, text) => {
-//     const element = document.getElementById(selector);
-//     if (element) element.innerText = text;
-//   };
-
-//   console.log("DOMContentLoaded");
-
-//   for (const dependency of ["chrome", "node", "electron"]) {
-//     replaceText(`${dependency}-version`, process.versions[dependency]);
-//   }
-// });
-
-window.addEventListener("DOMContentLoaded", () => {
+onDOMContentLoaded.subscribe(() => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
@@ -26,3 +13,19 @@ window.addEventListener("DOMContentLoaded", () => {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
 });
+
+fromElementEvent("#button", "click").subscribe(() => {
+  console.log("click");
+});
+
+function fromElementEvent(selector, eventName) {
+  return onDOMContentLoaded.pipe(
+    switchMap(() => {
+      const element = document.querySelector(selector);
+      if (element) {
+        return fromEvent(element, eventName);
+      }
+      return EMPTY;
+    })
+  );
+}
