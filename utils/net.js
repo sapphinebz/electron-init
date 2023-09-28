da15f295ec4033c78118a2cdc0f8cae9a3686939
sapphinebz <sapphinebz@gmail.com>
2023-09-28 08:00:45 +0700

    added node https

--- utils/net.js
+++ utils/net.js
@@ -1,5 +1,7 @@
 const { net } = require("electron");
 const { Observable } = require("rxjs");
+const { map } = require("rxjs/operators");
+const https = require("https");
 
 /**
  * Automatic management of system proxy configuration, support of the wpad protocol and proxy pac configuration files.
@@ -31,6 +33,52 @@ function httpGet(url) {
   });
 }
 
+function nodeHttps(url) {
+  return new Observable((subscriber) => {
+    const abortController = new AbortController();
+    https
+      .get(
+        url,
+        { signal: abortController.signal, method: "GET" },
+        (response) => {
+          let chunks = "";
+          response.on("data", (chunk) => {
+            chunks += chunk;
+          });
+          response.on("end", () => {
+            subscriber.next(chunks);
+            subscriber.complete();
+          });
+        }
+      )
+      .on("error", (error) => {
+        subscriber.error(error);
+      });
+
+    return {
+      unsubscribe: () => {
+        abortController.abort();
+      },
+    };
+  });
+}
+/**
+ *
+ * @param {string} url
+ * @returns Observable<string>
+ */
+function httpsImageBase64(url) {
+  return nodeHttps(url).pipe(
+    map((chunks) => {
+      const base64Data = Buffer.from(chunks).toString("base64");
+      const url = `data:image/svg+xml;base64,${base64Data}`;
+      return url;
+    })
+  );
+}
+
 module.exports = {
   httpGet,
+  nodeHttps,
+  httpsImageBase64,
 };
