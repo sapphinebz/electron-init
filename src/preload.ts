d9eb73e0948dba0b3dd5e18eddde09092e211738
sapphinebz <sapphinebz@gmail.com>
2023-10-07 20:43:37 +0700

    added listenIPCMain sendToIPCMain

--- src/preload.ts
+++ src/preload.ts
@@ -1,18 +1,32 @@
 // See the Electron documentation for details on how to use preload scripts:
 
 import { contextBridge, ipcRenderer } from "electron";
-import { Observable, fromEvent, shareReplay } from "rxjs";
-import { listenIPCMain } from "./ipc-renderer/listen-ipc-main";
 
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 contextBridge.exposeInMainWorld("electronAPI", {
-  sendToIPCMain: (eventName: string, arg: any): Promise<any> => {
+  sendAsyncToIPCMain: (eventName: string, arg: any): Promise<any> => {
     return new Promise((resolve) => {
       ipcRenderer.once(eventName, (event: Electron.IpcRendererEvent, arg) => {
         resolve(arg);
       });
       ipcRenderer.send(eventName, arg);
     });
+  },
+
+  sendToIPCMain: (eventName: string, ...args: any[]): void => {
+    ipcRenderer.send.call(ipcRenderer, eventName, ...args);
+  },
+
+  listenIPCMain: (eventName: string, handler: (...args: any[]) => void) => {
+    const proxyHandler = (event: Electron.IpcRendererEvent, ...args: any[]) => {
+      handler(args);
+    };
+    ipcRenderer.addListener(eventName, proxyHandler);
+    return ipcRenderer.removeListener.bind(
+      ipcRenderer,
+      eventName,
+      proxyHandler
+    );
   },
 
   sendSyncToIPCMain: (eventName: string, arg: any): any => {
